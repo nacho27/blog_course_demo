6995a546be25ec760802abd5c5c9757bc0ed7e99
susuaung <susuaung.ms@gmail.com>
2015-12-14 15:35:03 +0900

    remove migration

--- db/migrate/20151214055223_remove_slug_form_posts.rb
+++
@@ -1,4 +0,0 @@
-class RemoveSlugFormPosts < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20151214061409_remove_slug_from_posts.rb
+++
@@ -1,5 +0,0 @@
-class RemoveSlugFromPosts < ActiveRecord::Migration
-  def change
-    remove_column :posts, :slug, :string
-  end
-end
