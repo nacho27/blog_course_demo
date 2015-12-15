797594c503b57e52f61f7111fb56061699ee6902
susuaung <susuaung.ms@gmail.com>
2015-12-15 09:30:11 +0900

    Setup contact form

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
     resources :posts
     resources :projects
+    resources :contacts , only: [:new, :create]
     get 'welcome/index'
     root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
